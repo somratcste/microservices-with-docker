9c71ec62a1041607d4335ddc3c88d7f770309bac
somratcste <somratcste@gmail.com>
2019-12-28 11:28:12 +0100

    add full stack application for docker run

---
+++ 04-spring-boot-react-full-stack-h2/frontend/todo-app/build/precache-manifest.67f20be789284362fe9d794e77de2aab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c83f514f554c199c00e1",
+    "url": "/static/js/main.a9a8b9fe.chunk.js"
+  },
+  {
+    "revision": "3be61595d9a1137a984f",
+    "url": "/static/js/2.3f64e426.chunk.js"
+  },
+  {
+    "revision": "c83f514f554c199c00e1",
+    "url": "/static/css/main.2e9ff12e.chunk.css"
+  },
+  {
+    "revision": "1a2a84f9f309adf27a13caa871bbc49f",
+    "url": "/index.html"
+  }
+];
